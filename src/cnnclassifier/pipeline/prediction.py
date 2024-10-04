56afc219cbc5960c8545b295ecf83c4a900ee56c
Abubakar Saddiq <abubakarsaddiq0011992gmail.com>
2024-10-04 18:16:13 +0500

    testing

--- src/cnnclassifier/pipeline/prediction.py
+++ src/cnnclassifier/pipeline/prediction.py
@@ -35,3 +35,35 @@ class PredictionPipeline:
         prediction = self.class_labels.get(result[0], 'Unknown')
 
         return [{"image shows": prediction}]
+
+# import numpy as np
+# from tensorflow.keras.models import load_model  # type: ignore
+# from tensorflow.keras.preprocessing import image  # type: ignore
+# import os
+
+
+# class PredictionPipeline:
+#     def __init__(self, filename):
+#         self.filename = filename
+
+#     def predict(self):
+#         # Load the model
+#         model = load_model(os.path.join(
+#             "artifacts", "training", "model.keras"))
+
+#         # Load and preprocess the image
+#         imagename = self.filename
+#         test_image = image.load_img(imagename, target_size=(224, 224))
+#         test_image = image.img_to_array(test_image)
+#         test_image = np.expand_dims(test_image, axis=0)
+
+#         # Predict the class
+#         result = np.argmax(model.predict(test_image), axis=1)
+#         print(result)
+
+#         # Define the class mapping
+#         class_labels = {0: 'Early Blight', 1: 'Late Blight', 2: 'Healthy'}
+
+#         # Return the prediction
+#         prediction = class_labels.get(result[0], "Unknown")
+#         return [{"image": prediction}]
